905bbd1dbda99ea2714224d9d408218522f5248d
Xin Zhou <maiziex@gmail.com>
2017-10-31 10:26:13 -0700

    Update run_step1-5.sh

--- src/run_step1-5.sh
+++ src/run_step1-5.sh
@@ -11,7 +11,7 @@ python BasicXX_step1.py -l 100000000 -i ../S_24385_Lysis_2_USPD16081850_H3332CCX
 #python BasicXX_step4.py -o S_24385_L1. --out_dir ../ -r /scratch/users/xzhou15/refdata-GRCh38-2.1.0/fasta/genome.fa
 
 # step 5:
-#python BasicXX_step5.py -o Xin --out_dir ../ -b /scratch/users/xzhou15/SimProj_10X/RealData/Scripts/barcode4M.fa --h5_dir ../qual_20/
+#python BasicXX_step5.py -o Xin --out_dir ../ -b /oak/stanford/groups/arend/Xin/Scripts/barcode4M.fa --h5_dir ../qual_20/
 
 # step 6:
 #python BasicXX_step6.py --h5_dir ../../qual_20/
